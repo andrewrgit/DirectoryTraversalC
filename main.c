67fa61ba731a05797166bfa4ad162692d05167fd
riebow <asrm8d@mail.umsl.edu>
2020-02-03 22:39:03 -0600

    Modified main.c, added directory checking and loop to output file name. Getting struct stat from files

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
 
-void main(){
-	printf("%d", 4);
-
+void main(int argc, char *argv[]){
+	struct dirent *direntp;
+	DIR *dirp;
+	struct stat statbuf;
+	
+	if (argc != 2){
+		fprintf(stderr, "Usage: %s directory_name\n", argv[0]);
+		return;
+	}
+	dirp = opendir(argv[1]);
+	while ((direntp = readdir(dirp)) != NULL){
+		if(direntp->d_type==DT_UNKNOWN){
+		stat(direntp->d_name, &statbuf);
+		printf("%s Type: %u Stat: %d\n ", direntp->d_name, direntp->d_type, &statbuf.st_uid);
+		}
+	}
 }
