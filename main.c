7aa1926ac103f8fe232e8ab2fc1a68b30ba8187b
riebow <asrm8d@mail.umsl.edu>
2020-02-04 00:06:22 -0600

    Added option handling

--- main.c
+++ main.c
@@ -3,17 +3,45 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void main(int argc, char *argv[]){
 	struct dirent *direntp;
 	DIR *dirp;
 	struct stat statbuf;
 	
-	if (argc != 2){
-		fprintf(stderr, "Usage: %s directory_name\n", argv[0]);
+	opterr = 1;
+	int hFlag = 0;
+	int LFlag = 0;
+	int dFlag = 0;
+	int option;
+	
+	
+	while ((option = getopt (argc, argv, "hLdgipstul")) != -1){
+		switch(option){
+			case 'h':
+				hFlag = 1;
+				break;
+			case 'L':
+				LFlag = 1;
+				break;
+			case 'd':
+				dFlag = 1;
+				break;
+		}
+	}
+	
+	if (hFlag == 1){
+		printf("Usage: %s directory_name\n", argv[0]);
+		exit(0);
+	}
+	
+	if (argc - optind > 1){
+		fprintf(stderr, " Usage: %s directory_name\n", argv[0]);
 		return;
 	}
-	dirp = opendir(argv[1]);
+	dirp = opendir(argv[optind]);
+	printf("argv[1] is %s", argv[1]);
 	while ((direntp = readdir(dirp)) != NULL){
 		if(direntp->d_type==DT_UNKNOWN){
 		stat(direntp->d_name, &statbuf);
